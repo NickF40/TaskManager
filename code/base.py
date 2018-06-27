2a2138e1d16a685a3e4bc4dff7fbc2a367822fb2
AlexSaplin <40490358+AlexSaplin@users.noreply.github.com>
2018-06-27 17:18:53 +0500

    Moved from SQLite to PostgreSQL Database

--- code/base.py
+++ code/base.py
@@ -1,9 +1,7 @@
 """
-Need to move from SQLite to PostgreSQL Database -- что-то уже есть
-Think about merge tasks
+Need to move from SQLite to PostgreSQL Database -- DONE
 """
 
-import sqlite3 as sqlite
 from distutils import dist
 import time
 import random as rd
@@ -20,7 +18,7 @@ cur = db.cursor()
 #"CREATE TABLE users(user_id SERIAL, user_name TEXT, vk_uid INTEGER, tg_uid INTEGER);"
 
 def get_last_user_id():
-    cur.execute("SELECT * FROM users ORDER BY id DESC LIMIT 1;")
+    cur.execute("SELECT * FROM users ORDER BY user_id DESC LIMIT 1;")
     return cur.fetchone()
 
 
@@ -59,83 +57,41 @@ def add_user(user_name, mode, chat_uid):
     else:
         logging.critical('User already exists')
 
-"""
-Сорян, что на русском
-Че за quot???
-Нигде про это нет
-И kickass???
-У нас нет таблички motivate...
-"""
-
-def quot():
-    i = rd.randint(1, 767)
-    print(i)
-    db = sqlite.connect("C:/Users/Nick/Desktop/clientbase.db")
-    cur = db.cursor()
-    cur.execute("SELECT * FROM Quotations WHERE id = (?)", (i,))
-    string = cur.fetchone()
-    return (str(string[1]) + string[2])
-    db.close()
-
-
-def kickass():
-    i = rd.randint(1, 767)
-    print(i)
-    db = sqlite.connect("C:/Users/Nick/Desktop/clientbase.db")
-    cur = db.cursor()
-    cur.execute("SELECT * FROM motivate WHERE id = (?)", (i,))
-    db.close()
-
-"""
-Пошел осмысленный код
-"""
-
-#"CREATE TABLE task_history(id SERIAL PRIMARY KEY, task JSONB, uid INTEGER);"
+#"CREATE TABLE task_history(id SERIAL PRIMARY KEY, task JSONB, user_id INTEGER, task_result INTEGER);"
 
 def get_last_task_id():
     cur.execute("SELECT * FROM task_history ORDER BY id DESC LIMIT 1;")
     return cur.fetchone()
 
-"""
-Сюда придет class Task
-"""
 
-def add_task(user_id, task):
+def add_task_to_history(user_id, task, task_result):
     task_json = task.json()
     cur.execute("""
-    INSERT INTO task_history (task, uid)
-    VALUES (%s, %s);
+    INSERT INTO task_history (task, user_id, task_result)
+    VALUES (%s, %s, %s);
     """,
-    (task_json, user_id))
+    (task_json, user_id, task_result))
     db.commit()
 
-def set_todo(user_id, activity_name, start_time, finish_time, description, frequency):
-    db = sqlite.connect("C:/Users/Nick/Desktop/clientbase.db")
-    cur = db.cursor()
-    cur.execute("SELECT * FROM users WHERE user_id = ?", (str(user_id),))
-    is_todo = cur.fetchone()
-    if is_todo[5]:
-        cur.execute(
-            "INSERT INTO '{table}' (activity_name, start_time, finish_time, description, frequency)  VALUES (?,?,?,?,?)".format(table=user_id), (activity_name, start_time, finish_time, description, frequency,))
-        db.commit()
+
+def get_tasks_from_history(user_id, tasks_count=-1):
+    cur.execute("SELECT task, task_result FROM task_history WHERE user_id = %s;", (user_id,))
+    tasks = cur.fetchall()
+    real_tasks_count = len(tasks)
+    if (tasks_count == -1):
+        tasks_count = real_tasks_count
+    if (tasks_count <= real_tasks_count):
+        return ((task[0], task[1]) for task in cur.fetchall())[-tasks_count:]
     else:
-        s = '''CREATE TABLE {table} (id	INTEGER,activity_name	TEXT, start_time	REAL, finish_time	REAL, description	TEXT, frequency	INTEGER, PRIMARY KEY(id))'''.format(table=user_id)
-        cur.execute(s)
-        cur.execute("UPDATE users SET todos_t_name = {name} WHERE user_id = {id}".format(id=user_id, name=user_id))
-        db.commit()
-    db.close()
+        logging.critical("User doesn't have enough tasks")
 
 
-def get_todo(user_id):
-    db = sqlite.connect("clientbase.db")
-    cur = db.cursor()
-    try:
-        cur.execute("SELECT * FROM '{table}'".format(table=str(time.gmtime(time.time()).tm_mon)+'-'+str(time.gmtime(time.time()).tm_mday)))
-    except Exception as e:
-        print(e)
-        cur.execute("CREATE TABLE %s(id INTEGER PRIMARY KEY, activity_name TEXT,"
-                    "start_time REAL, finish_time REAL,"
-                    " description TEXT" % str(str(time.gmtime(time.time())[1])+'-'+str(time.gmtime(time.time())[2])))
-        return None
-    data = cur.fetchall()
-    return data
+def clear_task_history(user_id):
+    cur.execute("DELETE FROM task_history WHERE user_id = %s", (user_id,))
+    db.commit()
+
+
+def delete_user(user_id):
+    clear_task_history(user_id)
+    cur.execute("DELETE FROM users WHERE user_id = %s", (user_id,))
+    db.commit()
